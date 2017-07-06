b5310f08f4937fe0dd20351ab147c20183b4423b
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2017-07-06 18:30:20 +0000

    Connect to device

--- iocBoot/iocagilent33521a/st.cmd
+++ iocBoot/iocagilent33521a/st.cmd
@@ -7,9 +7,13 @@
 
 cd "${TOP}"
 
+epicsEnvSet("STREAM_PROTOCOL_PATH", "$(TOP)/agilent33521aApp/db")
+
 ## Register all support components
 dbLoadDatabase "dbd/agilent33521a.dbd"
 agilent33521a_registerRecordDeviceDriver pdbbase
+
+drvAsynIPPortConfigure("AGILENTPORT", "${IPADDR}:${IPPORT} TCP", 0, 0, 0)
 
 ## Load record instances
 #dbLoadRecords("db/xxx.db","user=janitoHost")
