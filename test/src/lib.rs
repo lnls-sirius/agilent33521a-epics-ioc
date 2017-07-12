f8ea4fdb5afac00646394956639ba23c20532044
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2017-07-12 19:39:16 +0000

    Create initial LineProtocol struct

--- test/src/lib.rs
+++ test/src/lib.rs
@@ -1,4 +1,6 @@
 extern crate bytes;
 extern crate tokio_io;
+extern crate tokio_proto;
 
 pub mod line_codec;
+pub mod line_protocol;
---
+++ test/src/line_protocol.rs
@@ -0,0 +1,30 @@
+use std::io;
+
+use tokio_io::{AsyncRead, AsyncWrite};
+use tokio_io::codec::Framed;
+use tokio_proto::pipeline::ServerProto;
+
+use line_codec::LineCodec;
+
+pub struct LineProtocol {
+    separator: u8,
+}
+
+impl LineProtocol {
+    pub fn with_separator(separator: u8) -> Self {
+        Self {
+            separator,
+        }
+    }
+}
+
+impl <T: AsyncRead + AsyncWrite + 'static> ServerProto<T> for LineProtocol {
+    type Request = String;
+    type Response = String;
+    type Transport = Framed<T, LineCodec>;
+    type BindTransport = io::Result<Self::Transport>;
+
+    fn bind_transport(&self, io: T) -> Self::BindTransport {
+        Ok(io.framed(LineCodec::with_separator(self.separator)))
+    }
+}
