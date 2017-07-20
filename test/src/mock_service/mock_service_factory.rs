b9f077e4f6c540d42dd1d7e2cb8b64f5a48abd41
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2017-07-20 11:58:30 +0000

    Separate MockServiceFactory into a new sub-module

---
+++ test/src/mock_service/mock_service_factory.rs
@@ -0,0 +1,50 @@
+use std::fmt::Display;
+use std::io;
+
+use tokio_service::NewService;
+
+use super::expected_request::ExpectedRequest;
+use super::mock_service::MockService;
+
+pub struct MockServiceFactory<A, B>
+where
+    A: Clone,
+    B: Clone,
+{
+    expected_requests: Vec<ExpectedRequest<A, B>>,
+}
+
+impl<A, B> MockServiceFactory<A, B>
+where
+    A: Clone,
+    B: Clone,
+{
+    pub fn new() -> Self {
+        Self {
+            expected_requests: Vec::new(),
+        }
+    }
+
+    pub fn expect(&mut self, request: A, response: B) {
+        let expected_request = ExpectedRequest { request, response };
+
+        self.expected_requests.push(expected_request);
+    }
+}
+
+impl<A, B> NewService for MockServiceFactory<A, B>
+where
+    A: Clone + Display + PartialEq,
+    B: Clone,
+{
+    type Request = A;
+    type Response = B;
+    type Error = io::Error;
+    type Instance = MockService<A, B>;
+
+    fn new_service(&self) -> io::Result<Self::Instance> {
+        let requests = self.expected_requests.clone();
+
+        Ok(Self::Instance::with_expected_requests(requests))
+    }
+}
--- test/src/mock_service/mod.rs
+++ test/src/mock_service/mod.rs
@@ -2,56 +2,9 @@ mod errors;
 mod expected_request;
 mod handle_request;
 mod mock_service;
-
-use std::fmt::Display;
-use std::io;
-
-use tokio_service::NewService;
+mod mock_service_factory;
 
 pub use self::errors::{Error, ErrorKind, Result};
-use self::expected_request::ExpectedRequest;
 pub use self::handle_request::HandleRequest;
 pub use self::mock_service::MockService;
-
-pub struct MockServiceFactory<A, B>
-where
-    A: Clone,
-    B: Clone,
-{
-    expected_requests: Vec<ExpectedRequest<A, B>>,
-}
-
-impl<A, B> MockServiceFactory<A, B>
-where
-    A: Clone,
-    B: Clone,
-{
-    pub fn new() -> Self {
-        Self {
-            expected_requests: Vec::new(),
-        }
-    }
-
-    pub fn expect(&mut self, request: A, response: B) {
-        let expected_request = ExpectedRequest { request, response };
-
-        self.expected_requests.push(expected_request);
-    }
-}
-
-impl<A, B> NewService for MockServiceFactory<A, B>
-where
-    A: Clone + Display + PartialEq,
-    B: Clone,
-{
-    type Request = A;
-    type Response = B;
-    type Error = io::Error;
-    type Instance = MockService<A, B>;
-
-    fn new_service(&self) -> io::Result<Self::Instance> {
-        let requests = self.expected_requests.clone();
-
-        Ok(Self::Instance::with_expected_requests(requests))
-    }
-}
+pub use self::mock_service_factory::MockServiceFactory;
