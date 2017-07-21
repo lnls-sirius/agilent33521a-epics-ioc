7e25c1b4cd1790575ff447c0b538880bc2017d22
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2017-07-21 12:52:51 +0000

    Move BoundConnectionFuture to a new sub-module

---
+++ test/src/mock_server/bound_connection_future/bound_connection_future.rs
@@ -0,0 +1,41 @@
+use std::sync::{Arc, Mutex};
+
+use futures::{Future, Poll};
+use tokio_core::net::{TcpListener, TcpStream};
+use tokio_proto::pipeline::ServerProto;
+
+use super::state::State;
+use super::super::connection_future::ConnectionFuture;
+use super::super::errors::Error;
+
+pub struct BoundConnectionFuture<P>
+where
+    P: ServerProto<TcpStream>,
+{
+    state: State<P>,
+}
+
+impl<P> BoundConnectionFuture<P>
+where
+    P: ServerProto<TcpStream>,
+{
+    pub fn from(listener: TcpListener, protocol: Arc<Mutex<P>>) -> Self {
+        let connection = ConnectionFuture::from(listener);
+
+        Self {
+            state: State::start_with(connection, protocol),
+        }
+    }
+}
+
+impl<P> Future for BoundConnectionFuture<P>
+where
+    P: ServerProto<TcpStream>,
+{
+    type Item = P::Transport;
+    type Error = Error;
+
+    fn poll(&mut self) -> Poll<Self::Item, Self::Error> {
+        self.state.advance()
+    }
+}
--- test/src/mock_server/bound_connection_future/mod.rs
+++ test/src/mock_server/bound_connection_future/mod.rs
@@ -1,43 +1,4 @@
 mod state;
+mod bound_connection_future;
 
-use std::sync::{Arc, Mutex};
-
-use futures::{Future, Poll};
-use tokio_core::net::{TcpListener, TcpStream};
-use tokio_proto::pipeline::ServerProto;
-
-use self::state::State;
-use super::connection_future::ConnectionFuture;
-use super::errors::Error;
-
-pub struct BoundConnectionFuture<P>
-where
-    P: ServerProto<TcpStream>,
-{
-    state: State<P>,
-}
-
-impl<P> BoundConnectionFuture<P>
-where
-    P: ServerProto<TcpStream>,
-{
-    pub fn from(listener: TcpListener, protocol: Arc<Mutex<P>>) -> Self {
-        let connection = ConnectionFuture::from(listener);
-
-        Self {
-            state: State::start_with(connection, protocol),
-        }
-    }
-}
-
-impl<P> Future for BoundConnectionFuture<P>
-where
-    P: ServerProto<TcpStream>,
-{
-    type Item = P::Transport;
-    type Error = Error;
-
-    fn poll(&mut self) -> Poll<Self::Item, Self::Error> {
-        self.state.advance()
-    }
-}
+pub use self::bound_connection_future::BoundConnectionFuture;
