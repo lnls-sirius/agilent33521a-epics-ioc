7e25e3704fcee941e8eed9a886fb91d99179b2b6
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2017-07-28 21:49:44 +0000

    Refactor IocInstance error handling
    - Use Option<Error> instead of Result<IocProcess> to make the code more
      readable

--- test/src/ioc/ioc_instance.rs
+++ test/src/ioc/ioc_instance.rs
@@ -3,45 +3,38 @@ use std::process::ExitStatus;
 
 use futures::{AsyncSink, Future, Poll, Sink};
 
-use super::errors::{Error, ErrorKind, Result};
+use super::errors::{Error, ErrorKind};
 use super::ioc_process::IocProcess;
 
 pub struct IocInstance {
-    process: Result<IocProcess>,
+    process: IocProcess,
+    error: Option<Error>,
 }
 
 impl IocInstance {
     pub fn new(process: IocProcess) -> Self {
         Self {
-            process: Ok(process),
+            process,
+            error: None,
         }
     }
 
     pub fn set_variable(&mut self, name: &str, value: &str) {
-        let set_variable_error = Err(ErrorKind::SettingIocVariable.into());
-        let write_error = Err(ErrorKind::IocWriteError.into());
-
-        let old_process_value =
-            mem::replace(&mut self.process, set_variable_error);
-
-        let new_process_value = if let Ok(mut process) = old_process_value {
+        if self.error.is_none() {
             let command = format!("dbpf {} {}\n", name, value);
+            let write_error = ErrorKind::IocWriteError.into();
 
-            match process.start_send(command.into()) {
-                Ok(AsyncSink::Ready) => Ok(process),
-                Ok(AsyncSink::NotReady(_)) => write_error,
-                Err(error) => Err(error.into()),
+            self.error = match self.process.start_send(command.into()) {
+                Ok(AsyncSink::Ready) => None,
+                Ok(AsyncSink::NotReady(_)) => Some(write_error),
+                Err(error) => error.into(),
             }
-        } else {
-            old_process_value
-        };
-
-        let _ = mem::replace(&mut self.process, new_process_value);
+        }
     }
 
     pub fn kill(&mut self) {
-        if let Ok(ref mut process) = self.process {
-            process.kill();
+        if self.error.is_none() {
+            self.process.kill();
         }
     }
 }
@@ -51,15 +44,18 @@ impl Future for IocInstance {
     type Error = Error;
 
     fn poll(&mut self) -> Poll<Self::Item, Self::Error> {
-        let poll_error = Err(ErrorKind::IocInstancePolledAfterEnd.into());
-        let old_process_value = mem::replace(&mut self.process, poll_error);
+        let temporary_error = ErrorKind::IocInstancePolledAfterEnd.into();
+        let error_status = mem::replace(&mut self.error, Some(temporary_error));
 
-        let (poll_result, new_process_value) = match old_process_value {
-            Ok(mut process) => (process.poll(), Ok(process)),
-            Err(error) => return Err(error),
+        let (poll_result, new_error_status) = match error_status {
+            None => (self.process.poll(), None),
+            Some(error) => (
+                Err(error),
+                Some(ErrorKind::IocInstancePolledAfterEnd.into()),
+            ),
         };
 
-        let _ = mem::replace(&mut self.process, new_process_value);
+        let _temporary_error = mem::replace(&mut self.error, new_error_status);
 
         poll_result
     }
