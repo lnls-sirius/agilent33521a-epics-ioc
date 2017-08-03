60d23c687cec0717663d664b3d83d122e19a155d
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2017-08-03 19:56:41 +0000

    Refactor Clone implementation for ExpectedRequest

--- test/src/mock_service/expected_request.rs
+++ test/src/mock_service/expected_request.rs
@@ -1,9 +1,17 @@
-#[derive(Clone, Default)]
-pub struct ExpectedRequest<A, B>
+pub struct ExpectedRequest<A, B> {
+    pub request: A,
+    pub response: B,
+}
+
+impl<A, B> Clone for ExpectedRequest<A, B>
 where
     A: Clone,
     B: Clone,
 {
-    pub request: A,
-    pub response: B,
+    fn clone(&self) -> ExpectedRequest<A, B> {
+        ExpectedRequest {
+            request: self.request.clone(),
+            response: self.response.clone(),
+        }
+    }
 }
