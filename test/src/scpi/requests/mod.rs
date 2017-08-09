91013a52b083d66f1708c53e4beb65a7151ca53c
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2017-08-09 12:34:19 +0000

    Implement Display for ScpiRequest

--- test/src/scpi/requests/mod.rs
+++ test/src/scpi/requests/mod.rs
@@ -1,3 +1,6 @@
+use std::fmt;
+use std::fmt::{Display, Formatter};
+
 use super::errors::{ErrorKind, Result};
 
 #[derive(Clone, Eq, Hash, PartialEq)]
@@ -6,5 +9,11 @@ pub enum ScpiRequest {}
 impl ScpiRequest {
     pub fn from(string: &str) -> Result<ScpiRequest> {
         Err(ErrorKind::UnknownScpiRequest(String::from(string)).into())
+    }
+}
+
+impl Display for ScpiRequest {
+    fn fmt(&self, formatter: &mut Formatter) -> fmt::Result {
+        Ok(())
     }
 }
