b99a7e3f4d3a640d28ee2759097d56ab30a2a3a8
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2017-08-09 13:12:01 +0000

    Implement Integer SCPI response type

--- test/src/scpi/response.rs
+++ test/src/scpi/response.rs
@@ -3,12 +3,16 @@ use bytes::BytesMut;
 #[derive(Clone)]
 pub enum ScpiResponse {
     Empty,
+    Integer(isize),
 }
 
 impl ScpiResponse {
-    pub fn encode(&self, _buffer: &mut BytesMut) {
+    pub fn encode(&self, buffer: &mut BytesMut) {
         match *self {
-            ScpiResponse::Empty => {},
+            ScpiResponse::Empty => {}
+            ScpiResponse::Integer(value) => {
+                buffer.extend(value.to_string().as_bytes())
+            }
         }
     }
 }
