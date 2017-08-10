6d9977c47536a8121eae2742bf0110a0e817be70
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2017-08-10 19:46:06 +0000

    Add Utf8String SCPI response message

--- test/src/scpi/response.rs
+++ test/src/scpi/response.rs
@@ -4,6 +4,7 @@ use bytes::BytesMut;
 pub enum ScpiResponse {
     Empty,
     Integer(isize),
+    Utf8String(String),
 }
 
 impl ScpiResponse {
@@ -12,6 +13,9 @@ impl ScpiResponse {
             ScpiResponse::Empty => {}
             ScpiResponse::Integer(value) => {
                 buffer.extend(value.to_string().as_bytes())
+            }
+            ScpiResponse::Utf8String(ref string) => {
+                buffer.extend(string.as_bytes())
             }
         }
     }
