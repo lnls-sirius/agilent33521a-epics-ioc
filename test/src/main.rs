400cea181bce2867bfa63e238b4cc1c76c3d13a0
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2017-08-11 17:22:37 +0000

    Color test report

--- test/src/main.rs
+++ test/src/main.rs
@@ -1,4 +1,8 @@
 extern crate agilent33521a_ioc_test;
+extern crate termion;
+
+use termion::color::{Fg, Green, Red, Yellow};
+use termion::style::{Bold, Reset};
 
 use agilent33521a_ioc_test::tests::run_tests;
 
@@ -13,25 +17,56 @@ fn main() {
                     Ok(_) => successful_tests += 1,
                     Err(ref error) => {
                         println!(
-                            " Test {} failed: {}",
-                            test_result.name(),
-                            error
+                            "{bold}{red}Fail: {reset}{yellow}{name}{reset}: \
+                             {message}",
+                            bold = Bold,
+                            name = test_result.name(),
+                            message = error,
+                            red = Fg(Red),
+                            yellow = Fg(Yellow),
+                            reset = Reset,
                         );
                         failed_tests += 1;
                     }
                 }
             }
 
+            if failed_tests > 0 {
+                println!("");
+            }
+
             if successful_tests == 1 {
-                println!("1 test succeeded");
+                println!(
+                    "{bold}1 test {green}succeeded{reset}",
+                    bold = Bold,
+                    green = Fg(Green),
+                    reset = Reset
+                );
             } else if successful_tests > 1 {
-                println!("{} tests succeeded", successful_tests);
+                println!(
+                    "{bold}{count} tests {green}succeeded{reset}",
+                    bold = Bold,
+                    count = successful_tests,
+                    green = Fg(Green),
+                    reset = Reset
+                );
             }
 
             if failed_tests == 1 {
-                println!("1 test failed");
+                println!(
+                    "{bold}1 test {red}failed{reset}",
+                    bold = Bold,
+                    red = Fg(Red),
+                    reset = Reset
+                );
             } else if failed_tests > 1 {
-                println!("{} tests failed", failed_tests);
+                println!(
+                    "{bold}{count} tests {red}failed{reset}",
+                    bold = Bold,
+                    count = failed_tests,
+                    red = Fg(Red),
+                    reset = Reset
+                );
             }
         }
         Err(error) => println!("Test execution failure: {}", error),
