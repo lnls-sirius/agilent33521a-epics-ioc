3661c88c9c1298aead86a55e58943ec59c7ff057
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2017-08-12 19:00:33 +0000

    Test disabling channel output

--- test/src/output.rs
+++ test/src/output.rs
@@ -13,4 +13,12 @@ tests! {
             .reply_with(ScpiResponse::Empty)
             .verify();
     }
+
+    test("disable channel output") {
+        test.set_variable("channelOutput-Sel", "OFF");
+
+        test.when(ScpiRequest::OutputOff(1))
+            .reply_with(ScpiResponse::Empty)
+            .verify();
+    }
 }
