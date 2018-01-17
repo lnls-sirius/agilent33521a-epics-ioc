cfbdf65fe1ec4604b27a1b3f6688b23d9999f5e5
andrei_pereira <andrei.pereira@lnls.br>
2018-01-17 13:00:01 -0200

    Fix startup scripts

--- iocBoot/iocagilent33521a/stAgilent33521a.cmd
+++ iocBoot/iocagilent33521a/stAgilent33521a.cmd
@@ -3,7 +3,7 @@
 
 ####################################################
 
-epicsEnvSet("STREAM_PROTOCOL_PATH", "$(TOP)/agilent33521aApp/db")
+epicsEnvSet("STREAM_PROTOCOL_PATH", "$(TOP)/agilent33521aApp/Db")
 
 ## Register all support components
 dbLoadDatabase("$(TOP)/dbd/agilent33521a.dbd",0,0)
@@ -12,7 +12,7 @@ agilent33521a_registerRecordDeviceDriver pdbbase
 drvAsynIPPortConfigure("AGILENTPORT", "${DEVICE_IP}:${DEVICE_PORT} TCP", 0, 0, 0)
 
 ## Load record instances
-dbLoadRecords("db/agilent33521a.db","P=${P}, R=${R}, PORT=AGILENTPORT")
+dbLoadRecords("$(TOP)/agilent33521aApp/Db/agilent33521a.db","P=${P}, R=${R}, PORT=AGILENTPORT")
 
 < save_restore.cmd
 
