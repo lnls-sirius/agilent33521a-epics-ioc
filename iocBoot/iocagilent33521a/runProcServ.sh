d5633be90ee7a825ceb4e01a7e6514d528e6575e
andrei_pereira <andrei.pereira@lnls.br>
2018-02-05 13:48:22 -0200

    iocBoot/*/runProcServ.sh: use specified TELNET port
    if not set, use the previous 20000 default.

--- iocBoot/iocagilent33521a/runProcServ.sh
+++ iocBoot/iocagilent33521a/runProcServ.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 
+# Use default if not set
+if [ -z "${AGILENT33521A_DEVICE_TELNET_PORT}" ]; then
+    AGILENT33521A_DEVICE_TELNET_PORT=20000
+fi
+
 # Run run*.sh scripts with procServ
-/usr/local/bin/procServ -f -n agilent33521a -i ^C^D 20000 ./runAgilent33521a.sh "$@"
+/usr/local/bin/procServ -f -n agilent33521a${AGILENT33521A_INSTANCE} -i ^C^D ${AGILENT33521A_DEVICE_TELNET_PORT} ./runAgilent33521a.sh "$@"
