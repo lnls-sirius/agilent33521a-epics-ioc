2d6604b8b9429be69a6c06151668507fc44f10e7
Lucas Russo <lerwys@gmail.com>
2018-11-23 12:49:28 -0200

    agilent33521aApp: add autosave monitor each 5 seconds

--- iocBoot/iocagilent33521a/stAgilent33521a.cmd
+++ iocBoot/iocagilent33521a/stAgilent33521a.cmd
@@ -26,4 +26,5 @@ iocInit
 
 # Create manual trigger for Autosave
 create_triggered_set("auto_settings_agilent33521a.req", "${P}${R}SaveTrg", "P=${P}, R=${R}")
+create_monitor_set("auto_settings_agilent33521a.req", 5, "P=${P}, R=${R}")
 set_savefile_name("auto_settings_agilent33521a.req", "auto_settings_${P}${R}.sav")
